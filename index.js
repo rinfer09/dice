b1080091c5434f06e542964d02c1233085685781
Dennis Ferdian <58701698+rinfer09@users.noreply.github.com>
2020-08-31 17:02:17 +0700

    first commit add files

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        document.querySelector(".quotes").innerHTML = "Who wins?"
+        let player1Number,player2Number;
+        const interval = setInterval(() => {
+            document.body.classList.toggle("background-change");
+            player1Number = Math.floor(Math.random() * 6) + 1;
+            player2Number = Math.floor(Math.random() * 6) + 1;
+
+            player1Dice = "dice" + player1Number + ".png";
+            player2Dice = "dice" + player2Number + ".png";
+
+            document.querySelector(".img1").setAttribute("src", "images/" + player1Dice);
+            document.querySelector(".img2").setAttribute("src", "images/" + player2Dice);
+
+            
+        }, 100)
+
+        setTimeout(() => {
+            clearInterval(interval)
+            var quotes = ""
+            if (player1Number > player2Number){
+                quotes = "Player 1 Wins!";
+            }else if (player1Number < player2Number){
+                quotes = "Player 2 Wins!";
+            }else{
+                quotes = "Draw!"
+            }
+
+            document.querySelector(".quotes").innerHTML = quotes;
+        },1000)
+
+        
+    }
+})
